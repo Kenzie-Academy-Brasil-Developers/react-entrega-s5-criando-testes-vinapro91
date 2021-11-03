80fa22cb534adc3b155ec2165cc68478cf8522ed
vinicius <vinapro91@gmail.com>
2021-11-03 11:12:28 -0300

    fix:remove no used hooks

--- src/__tests__/components/input.spec.tsx
+++ src/__tests__/components/input.spec.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Search from "../../components/Search/index";
-import MockAdapter from "axios-mock-adapter";
-import api from "../../services";
-import Providers from "../../providers";
-
-const apiMock = new MockAdapter(api);
 
 describe("search components:", () => {
   test("should be able to render an input", () => {
--- src/__tests__/pages/serarch.spec.tsx
+++ src/__tests__/pages/serarch.spec.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 import Search from "../../components/Search/index";
-import MockAdapter from "axios-mock-adapter";
-import api from "../../services";
 import Providers from "../../providers";
 
-const apiMock = new MockAdapter(api);
-
 describe("Search Page", () => {
-  apiMock.onGet("81630180").replyOnce(200, {});
   test("Should search by CEP", async () => {
     render(
       <Providers>
